964c91f5eb1169e59409dbe926041d3703639b00
yuekcc <yuekcc@qq.com>
2021-03-06 14:13:15 +0800

    chore: 增加使用  jsx 的子组件例子

--- List.jsx
+++ List.jsx
@@ -1,11 +1,16 @@
 // 导入 h 函数，用于 jsx 支持
 import { h } from "@/reactlike";
 
+function Item(props) {
+  return <button onclick={props.onclick}>{props.label}</button>;
+}
 export function List(props) {
+  const onItemClick = (x) => console.log(`click on item: `, x);
+
   return (
     <div>
       {props.data.map((it) => (
-        <button onclick={() => console.log(`click on ${it}`)}>{it}</button>
+        <Item onclick={() => onItemClick(it)} label={it}></Item>
       ))}
     </div>
   );
